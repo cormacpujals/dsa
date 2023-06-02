24c55dea2e3b5a6a5b44b56f44bb57b1e520632a
Cormac Pujals <110591827+cormacpujals@users.noreply.github.com>
2023-06-01 17:14:13 -0700

    fixes extra curly brace in crystal balls algo

--- src/day1/TwoCrystalBalls.ts
+++ src/day1/TwoCrystalBalls.ts
@@ -9,11 +9,11 @@ export default function two_crystal_balls(breaks: boolean[]): number {
 	i -= jmpAmount; // go back one jump (to then walk at worst sqrt(n) length to the first truthy value
 
 	for (let j = 0; j < jmpAmount && i < breaks.length; j++, ++i) {
-		if (breaks[i] {
+		if (breaks[i]) {
 			return i; // return the first truthy value --> ++i so it's the index at which the crystal ball breaks
 		}
 	}
 	return -1;
 }
 
-}
+
