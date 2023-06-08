15a0f50c742b1493a85592a547c8527af9ec6e82
Cormac Pujals <110591827+cormacpujals@users.noreply.github.com>
2023-06-08 16:30:15 -0700

    implements queue and singly linked list

--- src/day1/SinglyLinkedList.ts
+++ src/day1/SinglyLinkedList.ts
@@ -1,3 +1,8 @@
+// Linked list makes insertion and deletion a constant time operation. However, if the insertion or deletion is in the middle
+// of the linked list, traversal may be costly (traverse n-nodes to access, where in arrays, access is constant time. 
+// Linked lists and be singly or doubly linked. In a doubly linked list, nodes have a value, a previous property 
+// (that points to the previous node) and a next property.
+
 export default class SinglyLinkedList<T> {
     public length: number;
 
--- src/practice/allDSA/LinearSearchList.ts
+++ src/practice/allDSA/LinearSearchList.ts
@@ -1,9 +1,7 @@
-// O(n)
+// O(n) edit 
 export default function linear_search(haystack: number[], needle: number): boolean {
 	for (let i = 0; i < haystack.length; i++) {
-		if (haystack[i] === needle) {
-			return true;
-		}
+		if (haystack[i] === needle) return true;
 	}
 	return false;
 }
