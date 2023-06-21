e3b818a927c4de96a9e0677b8988c4796282ab2a
Cormac Pujals <110591827+cormacpujals@users.noreply.github.com>
2023-06-21 11:51:40 -0700

    implements class methods, i.e., not instance methods

--- src/practice/udemyDSA/basics.js
+++ src/practice/udemyDSA/basics.js
@@ -12,17 +12,17 @@
 
 // Student class example
 
-class Student {
-  constructor(firstName, lastName, year) {
-    this.firstName = firstName;
-    this.lastName = lastName;
-    this.grade = year;
-  }
-}
+// class Student {
+//   constructor(firstName, lastName, year) {
+//     this.firstName = firstName;
+//     this.lastName = lastName;
+//     this.grade = year;
+//   }
+// }
 
 // the new operator will help create an instance of a class (which is just syntactic sugar for a user-defined object with a constructor function)
 
-let firstStudent = new Student("Cormac", "Pujals");
+// let firstStudent = new Student("Cormac", "Pujals");
 
 // -> Student { firstName: "Cormac", lastName: "Pujals", grade: undefined }
 
@@ -51,7 +51,48 @@ class Student {
   }
   // challenge, implement add average score
   calculateAverage() {
-    return this.scores.reduce((acc, cur) => (acc / this.scores.length) + cur, 0);
+    return this.scores.reduce((acc, cur) => acc + (cur / this.scores.length), 0);
+  }
+  // class methods: utility functions in a class that pertain to all instances of the class, 
+  // e.g., adding a function to send a confirmation email that a student has been successfully enrolled
+  static enrollStudents(...students) {
+    return console.log("Enrolling students now...");
   }
 }
 
+let firstStudent = new Student("Connor", "Pujals", 16);
+let secondStudent = new Student("Cormac", "Pujals", 17);
+
+
+console.log(secondStudent);
+
+secondStudent.addScore(50);
+secondStudent.addScore(80);
+secondStudent.addScore(80);
+secondStudent.addScore(100);
+secondStudent.addScore(100);
+
+console.log(secondStudent.calculateAverage());
+
+// class method called via the following code: 
+Student.enrollStudents(firstStudent, secondStudent);
+
+
+class Point {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+  static distance(a, b) {
+    let xLength = a.x - b.x;
+    let yLength = a.y - b.y;
+    return Math.hypot(xLength, yLength);
+  }
+}
+
+let p1 = new Point(5,5);
+let p2 = new Point(12, 12);
+
+console.log(Point.distance(p1, p2));
+
+// class methods relate to all instances and instance methods are to be used for a specific instance of a class (and generally don't require data from other instances)
